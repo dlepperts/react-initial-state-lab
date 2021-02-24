16dbd62d5be945072e916b2ebb35c4a4894b500e
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-02-24 12:51:44 -0600

    changes

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 class ImageSlider extends React.Component {
     
+    constructor() {
+        
+        super() 
+    
+        this.state = {
+          currentSlideIndex: 0
+        }
+    }
+
+    
+    
 }
 
 export default ImageSlider;
